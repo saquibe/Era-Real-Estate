116e59fb547bbcf8c5f7c3f7ef4c1b1ece4f6617
Mohammad Saquib <saquibe143@gmail.com>
2024-09-06 01:49:00 +0530

    complete sign-in functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -18,13 +18,18 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
+        // Checking if the user exists
         const validUser = await User.findOne({ email });
         if(!validUser) return next(errorHandler(404, 'User not found'));
-        const validPassword = bcryptjs.compareSync.apply(password, validUser.password);
+         // Password comparison
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        // JWT token generation
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
         const { password: pass, ...rest} = validUser._doc;
-        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+        res.cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
     } catch (error) {
         next(error);
     }
