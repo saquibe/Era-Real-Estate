e60328b4d21acd1d64ad654a9b995a2902c7c8e4
Mohammad Saquib <saquibe143@gmail.com>
2024-09-08 22:38:27 +0530

    complete update user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -21,10 +21,22 @@ const userSlice = createSlice({
         singInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
-        }
-    }
+        },
+        updateUserStart: (state) => {
+            state.loading = true;
+        },
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+    },
 });
 
-export const { signInStart, singInSuccess, singInFailure } = userSlice.actions;
+export const { signInStart, singInSuccess, singInFailure, updateUserStart, updateUserSuccess, updateUserFailure } = userSlice.actions;
 
 export default userSlice.reducer;
